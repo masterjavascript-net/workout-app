f01ad06c4464f5c8f37f26819c59a79d33737ed3
Adil Valizada <adilvalizada@gmail.com>
2024-09-08 15:54:24 +0400

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    ></View>
+  );
+}
