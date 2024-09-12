322b4e13e7b515bfe619abbc31fd6567869a59dd
Adil Valizada <adilvalizada@gmail.com>
2024-09-12 12:51:11 +0400

    feat: updated navigation items

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,15 @@
+import AppLayouts from '@/components/AppLayouts.layout';
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+const Explore = () => {
+  return (
+    <AppLayouts>
+      <Text>Explore</Text>
+    </AppLayouts>
+  );
+};
+
+export default Explore;
+
+const styles = StyleSheet.create({});
